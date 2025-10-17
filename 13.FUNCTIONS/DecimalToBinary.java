a7616b9d1c017aeafed896e6fa27cad88f94d5a1
TekulaChandana <tekula.chandana.mca@gmail.com>
2025-10-17 21:41:49 +0530

    Added access specifiers and getter/setter methods in OOPs folder

--- 13.FUNCTIONS/DecimalToBinary.java
+++ 13.FUNCTIONS/DecimalToBinary.java
@@ -5,11 +5,14 @@ public class DecimalToBinary {
         while (decNum > 0) {
             int lastDigit = decNum % 2;
             decNum = decNum / 2;
+            System.out.println(lastDigit);
         }
+        return 0;
     }
 
     public static void main(String[] args) {
         int decNum = 121;
+        decimalBinary(decNum);
 
     }
 }
---
+++ 14.OOPS/AccessM.java
@@ -0,0 +1,17 @@
+public class AccessM {
+    public static void main(String[] args) {
+        BankAccount myAcc = new BankAccount();
+        myAcc.username = "Chandana";
+        myAcc.setPassword("abcd");
+    }
+
+}
+
+class BankAccount {
+    public String username;
+    private String password;
+
+    public void setPassword(String pwd) {
+        password = pwd;
+    }
+}
