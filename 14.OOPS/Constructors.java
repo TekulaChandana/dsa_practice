30ab4784847a9a16989920349a04ba7290183b00
TekulaChandana <tekula.chandana.mca@gmail.com>
2025-10-25 00:49:06 +0530

    copy constructor added

--- 14.OOPS/Constructors.java
+++ 14.OOPS/Constructors.java
@@ -2,6 +2,10 @@ public class Constructors {
   public static void main(String[] args) {
     Student s1 = new Student();
     Student s2 = new Student("Chandana");
+    s2.rollno = 132;
+    s2.age = 25;
+    Student s3 = new Student(s2);
+    s3.printInfo();
 
   }
 }
@@ -19,5 +23,18 @@ class Student {
   // Parametrized constructor
   Student(String name) {
     this.name = name;
+  }
+
+  // Copy Constructor
+  Student(Student s2) {
+    this.name = s2.name;
+    this.rollno = s2.rollno;
+    this.age = s2.age;
+  }
+
+  void printInfo() {
+    System.out.println(this.name);
+    System.out.println(this.rollno);
+    System.out.println(this.age);
   }
 }
