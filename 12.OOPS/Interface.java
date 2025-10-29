7ff0a851d4e9ea131c7dbd29b0721ca812cc9420
TekulaChandana <tekula.chandana.mca@gmail.com>
2025-10-29 22:40:18 +0530

    added interface

---
+++ 12.OOPS/Interface.java
@@ -0,0 +1,28 @@
+interface ChessPlayer {
+  void moves();
+}
+
+class Queen implements ChessPlayer {
+  public void moves() {
+    System.out.println("up, down, left, right, digonal(in all 4 directions)");
+  }
+}
+
+class Rook implements ChessPlayer {
+  public void moves() {
+    System.out.println("up,down,left,right");
+  }
+}
+
+class King implements ChessPlayer {
+  public void moves() {
+    System.out.println("up,down,left,right,diagonal-(by 1 step)");
+  }
+}
+
+public class Interface {
+  public static void main(String[] args) {
+    Queen q = new Queen();
+    q.moves();
+  }
+}
